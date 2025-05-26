6fb1da86c56b10c4179b53d79495d1636d4b4ad3
cnbbin <1624861606@qq.com>
2025-05-26 18:50:31 +0800

    init

--- cycledata/cond_demo.go
+++ cycledata/cond_demo.go
@@ -36,7 +36,7 @@ func GameInfo(){
             MiscData:   make(map[string]interface{}),
         }
     })
-    RegisterCreator(DailyCycle, TypeKey(1), func(userID)(*PlayerData){
+    RegisterCreator(DailyCycle, TypeKey(1), func(userID UserID)(*PlayerData){
         // 从数据库或缓存加载数据，示例返回空数据
         return &PlayerData{
             UserID:     userID,
