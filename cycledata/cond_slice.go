3c6f71a6fb96a663f7d0e1bb42d3f36ff2288ab2
cnbbin <1624861606@qq.com>
2025-05-28 16:37:19 +0800

    init

--- cycledata/cond_slice.go
+++ cycledata/cond_slice.go
@@ -47,14 +47,63 @@ func AppendToInt32SliceIf(cycle CycleType, typeKey TypeKey, userID UserID, key s
 }
 
 /*
- * RemoveFromInt32SliceIf 尝试从指定 []int32 类型的键值中删除元素 val
+ * AppendWithCDToInt32SliceIf 尝试向指定 []int32 类型的键值添加元素 val
  * 条件：
+  *  - lastUpdateLimitSec 与上次cd间隔限制
+ *   - val 不存在于切片中
+ *   - cond(slice) 返回 true
+ * 添加成功后更新时间
+ * 返回是否添加成功
+*/
+func AppendWithCDToInt32SliceIf(cycle CycleType, typeKey TypeKey, userID UserID, key string, val int32, lastUpdateLimitSec int, cond func([]int32) bool) bool {
+	pd := GetData(cycle, typeKey, userID)
+	if pd == nil {
+		return false
+	}
+
+	pd.mu.Lock()
+	defer pd.mu.Unlock()
+
+	raw, ok := pd.MiscData[key]
+	var slice []int32
+
+	if ok {
+		slice, ok = raw.([]int32)
+		if !ok {
+			// 字段存在，但不是[]int32类型，直接返回false
+			return false
+		}
+	} else {
+		// 字段不存在，初始化一个空切片
+		slice = []int32{}
+	}
+	// 检查时间间隔
+	if lastUpdateLimitSec > 0 {
+		if time.Since(pd.UpdateTime) > time.Duration(lastUpdateLimitSec)*time.Second {
+			return false
+		}
+	}
+	if !cond(slice) {
+		return false
+	}
+
+	// 满足条件，追加值
+	slice = append(slice, val)
+	pd.MiscData[key] = slice
+	pd.UpdateTime = time.Now()
+	return true
+}
+
+/*
+ * RemoveWithCDFromInt32SliceIf 尝试从指定 []int32 类型的键值中删除元素 val
+ * 条件：
+*  - lastUpdateLimitSec 与上次cd间隔限制
  *   - val 存在于切片中
  *   - cond(slice) 返回 true
  * 删除成功后更新时间
  * 返回是否删除成功
- */
-func RemoveFromInt32SliceIf(cycle CycleType, typeKey TypeKey, userID UserID, key string, val int32, cond func([]int32) bool) bool {
+*/
+func RemoveWithCDFromInt32SliceIf(cycle CycleType, typeKey TypeKey, userID UserID, key string, val int32, lastUpdateLimitSec int, cond func([]int32) bool) bool {
 	pd := GetData(cycle, typeKey, userID)
 	if pd == nil {
 		return false
@@ -73,7 +122,12 @@ func RemoveFromInt32SliceIf(cycle CycleType, typeKey TypeKey, userID UserID, key
 		// 字段存在，但不是[]int32类型，直接返回false
 		return false
 	}
-
+	// 检查时间间隔
+	if lastUpdateLimitSec > 0 {
+		if time.Since(pd.UpdateTime) > time.Duration(lastUpdateLimitSec)*time.Second {
+			return false
+		}
+	}
 	if !cond(slice) {
 		return false
 	}
